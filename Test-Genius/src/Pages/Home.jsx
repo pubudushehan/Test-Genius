acc30b15b9980eaf58354fe851a682101a1909e9
Pubudu_Shehan <110683869+pubudushehan@users.noreply.github.com>
2024-11-23 00:43:29 +0530

    create navbar and welcome components

---
+++ Test-Genius/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../component/NavBar";
+import Welcome from "../component/Welcome";
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+      <Welcome />
+    </div>
+  );
+};
+
+export default Home;
