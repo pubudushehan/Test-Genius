2bd0b2392c98ba4a5c465297a1dd1d8cb2bbc2c3
Pubudu_Shehan <110683869+pubudushehan@users.noreply.github.com>
2024-11-24 15:40:07 +0530

    Create Quiz Page

--- Test-Genius/src/Pages/Quiz.jsx
+++ Test-Genius/src/Pages/Quiz.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import NavBar from "../component/NavBar";
+import Footer from "../component/Footer";
+import QuizDo from "../component/QuizDo";
 
 const Quiz = () => {
-  return <div>Quiz</div>;
+  return <div>
+    <NavBar showlogin={false} />
+    <QuizDo />
+    <Footer />
+  </div>;
 };
 
 export default Quiz;
