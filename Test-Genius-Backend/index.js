9056fa09482771ff3ae250c42b82d30610b517c0
Pubudu_Shehan <110683869+pubudushehan@users.noreply.github.com>
2025-08-16 23:41:51 +0530

    fix: Docker issues

--- Test-Genius-Backend/index.js
+++ Test-Genius-Backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 // CORS configuration
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: ["http://localhost:3000", "http://localhost:5173" , "http://localhost"],
     credentials: true,
   })
 );
